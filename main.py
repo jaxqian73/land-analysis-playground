827091c9f76e22019dbe776f6d0211c46364c08d
jaxqian73 <jax.qian@gmail.com>
2026-02-10 05:24:13 +0000

    first python test

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hello Jax 👋")
+print("This is my first browser-based Python program.")
+
+land_price = 1800000
+build_cost = 1200000
+total_cost = land_price + build_cost
+
+print("Total project cost is:", total_cost)
